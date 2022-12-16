62378e41714ff7aa172ee725f8c6e03e0b61fefd
tladbwls <zipte@naver.com>
2022-12-16 12:48:30 +0900

    1216back

--- model/register.php
+++ model/register.php
@@ -1,11 +1,20 @@
 <?php
+    include $_SERVER['DOCUMENT_ROOT'].'/main)backend/etc/error/php';
 
     include_once $_SERVER['DOCUMENT_ROOT'].'/main_backend/connect/dbconn.php';
 
     if($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['req_sign'] == "signup") {
         signup($conn);
     }
-    
+
+    if($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['req_sign'] == "signin") {
+        signin($conn);
+    }
+
+    if($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['q']) && $_GET['q'] == "signout") {
+        signout();
+    }
+
     
 
     function signup ($conn) { //회원가입 처리 함수
@@ -45,7 +54,64 @@
 
         // echo json_encode(array("name" => $name, "id" => $id, "email" => $email, "pwd" => $pwd)); //문자열을 json 배열 변수로 반환한다. 파라미터에는 반드시 배열이 있어야 한다.
     }
-    function signin(){} //로그인 처리 함수
-    function signout(){} //로그아웃 처리 함수
+    
+    function signin($conn){ //로그인 처리 함수
+
+        //로그인 로직
+        //1. 받아온 아이디와 데이터베이스에 존재하는 아이디 비교
+        //2. 아이디가 없으면 없다는 메시지 전달
+        //3. 아이디가 존재하면 비밀번호와 데이터베이스에 존재하는 비밀번호 비교
+        //4. 비밀번호가 일치하지 않으면 없는 비번 메시지 전달
+        //5. 비밀번호가 일치하면 필요한 값 세션 저장
+        
+
+
+        $userid = $_POST['id'];
+        $pwd = $_POST ['pwd'];
+
+        $sql = "SELECT * FROM spl_user WHERE user_id = ?";
+        $stmt = $conn->stmt_init();
+
+        $stmt->prepare($sql);
+        $stmt->bind_param('s',$userid);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if (!mysqli_num_rows($result)) {
+            echo json_encode(array("login_msg" => "존재하지 않는 아이디입니다."));
+        } else {
+            $login_data = $result->fetch_array(); //회원 데이터 전체 추출하여 저장
+
+            $pwd_valid = password_verify($pwd, $login_data['user_pass']); //입력된 데이터(첫번째 파라미터)와 디비의 해시 데이터(두번째 파라미터)를 비교하여 boollean 값으로 변함
+
+            if (!$pwd_valid) {
+                echo json_encode(array("login_msg" => "비밀번호가 틀립니다."));
+            } else {
+                session_start();
+                $_SESSION['userid'] = $userid;
+                echo json_encode(array("userid" => $_SESSION['userid']));
+
+            }
+            
+            // echo json_encode((array("userid"=>$pwd_valid))); 
+
+            
+            // echo json_encode(array("userid" => $login_data));
+        }
+        
+
+        // echo $userid, $pwd;
+        // echo json_encode(array("userid" => mysqli_num_rows($result)));
+
+    } //로그인 처리 함수
+
+    function signout(){
+        if (isset($_SESSION['userid'])) {
+           session_unset();
+           session_destroy();
+        } 
+        
+    } //로그아웃 처리 함수
 
 ?>
